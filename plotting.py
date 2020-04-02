021aae3f39fd6072ff3d3e2f6f4f4ba427f83f86
Jan <jan.prosi@hotmail.com>
2020-04-02 22:52:16 +0200

    Colouring in animation changed

--- plotting.py
+++ plotting.py
@@ -213,19 +213,30 @@ def __plot_organism_init(settings, x1, y1, theta, energy, ax, type):
     # else:
     if type == 'pred':
         org_radius = settings['pred_radius']
+
+        circle_edge_color = 'red'
+        circle_face_color = 'firebrick'
+        edge_edge_color = 'maroon'
+        tail_color = 'g'
+
     elif type == 'prey':
         org_radius = settings['prey_radius']
+
+        circle_edge_color = 'g'
+        circle_face_color = 'lightgreen'
+        edge_edge_color = 'darkgreen'
+        tail_color = 'red'
 
 
     org_size_energy = org_radius * (np.log(energy + 1))
         #  If energy model is not active the "extract_plot_information function in embodied ising defines fitness thus#  foods eaten as energy
 
 
-    circle = Circle([x1,y1], org_radius, edgecolor = 'g', facecolor = 'lightgreen', zorder=8)
+    circle = Circle([x1,y1], org_radius, edgecolor = circle_edge_color, facecolor = circle_face_color, zorder=8)
     #circle = Circle([x1,y1], org_size, edgecolor = color1, facecolor = color1, zorder=8)
     ax.add_artist(circle)
 
-    edge = Circle([x1,y1], org_radius, facecolor='None', edgecolor = 'darkgreen', zorder=8)
+    edge = Circle([x1,y1], org_radius, facecolor='None', edgecolor = edge_edge_color, zorder=8)
     #edge = Circle([x1, y1], org_size, facecolor='None', edgecolor=color2, zorder=8)
     ax.add_artist(edge)
 
@@ -235,7 +246,7 @@ def __plot_organism_init(settings, x1, y1, theta, energy, ax, type):
     y2 = sin(radians(theta)) * tail_len + y1
 
 
-    ax.add_line(lines.Line2D([x1,x2],[y1,y2], color='red', linewidth=1, zorder=10))
+    ax.add_line(lines.Line2D([x1,x2],[y1,y2], color=tail_color, linewidth=1, zorder=10))
     #ax.add_line(lines.Line2D([x1, x2], [y1, y2], color='darkgreen', linewidth=1, zorder=10))
 
     pass
